67eb72feca748d7b88971b5c6cd8e60781a2022e
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2024-06-27 16:59:46 -0300

    V1

---
+++ Js/scripts.js
@@ -0,0 +1,52 @@
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+let currentImageIndex = 0;
+
+const images = [
+    'imagens/fotos/tibia1.jpg',
+    'imagens/fotos/tibia2.jpg',
+    'imagens/fotos/tibia3.jpg',
+];
+
+function openLightbox(index) {
+    currentImageIndex = index;
+    lightbox.style.display = 'block';
+    lightboxImg.src = images[currentImageIndex];
+}
+
+function closeLightbox() {
+    lightbox.style.display = 'none';
+}
+
+function changeImage(direction) {
+    currentImageIndex += direction;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    } else if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    lightboxImg.src = images[currentImageIndex];
+}    // Lightbox
+   
+
+    // Sidebar Menu
+    const menuIcon = document.querySelector('.menu-icon span');
+    const navLinks = document.querySelector('.nav-links');
+    menuIcon.addEventListener('click', () => {
+        navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!navLinks.contains(e.target) && !menuIcon.contains(e.target)) {
+            navLinks.style.display = 'none';
+        }
+    });
+
+    // Accordion
+    const accordionItems = document.querySelectorAll('.accordion-item h3');
+    accordionItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const content = item.nextElementSibling;
+            content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        });
+    });
